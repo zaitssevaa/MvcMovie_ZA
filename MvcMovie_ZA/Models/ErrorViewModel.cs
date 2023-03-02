e714ce758563cef15559339d95a888fad3baacbf
zaits <zaits@LAPTOP-CFRJMOCE>
2023-03-02 20:23:07 +0300

    Добавьте файлы проекта.

---
+++ MvcMovie_ZA/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace MvcMovie_ZA.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
